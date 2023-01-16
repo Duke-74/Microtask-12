f865dcbe3ac481042f139fcbef3a6a1fafeaf852
Maxim <belyanin-max@mail.ru>
2023-01-16 12:04:35 +0300

    Broken commit

--- tests/main.test.js
+++ tests/main.test.js
@@ -6,7 +6,7 @@ describe('Test', () => {
     request(app)
       .get('/post')
       .expect('Content-Type', /json/)
-      .expect(200)
+      .expect(400)
       .end((err, res) => {
         if (err) return done(err)
         return done()
